ba6f8264c2e98ace6e8f5cc38d092f02ea3e924c
NickSpringall <nick@fnqpianos.com.au>
2023-04-30 02:19:20 +1000

    comments for checkout function outline

--- checkout_functions.py
+++ checkout_functions.py
@@ -1,9 +1,38 @@
 def checkout():
     pass
-# get user info and save to XML
+# get user info and save to user_cart.XML
+    # first name
+    # last name
+    # phone number
+    # shipping address 
+        # street
+        # suburb
+        # state
+        # country
+
+# update XML name to user's actual name_cart
 
 # calculate shipping from info
+    # ask if they want express or standard
+        # figure out a state by state shipping rate per kg and make a function
+        # calculate total
+        # save to cart.xml
 
 # print summary of order
 
+    # customer numer and details
+
+    # product names
+    # product quantity
+    # unit cost
+    # total cost
+
+    # goods total
+    # shipping type and cost
+    # grand total
+
+# ask for confirmation of details and order
+
+
 # modify left in stock numbers
+# generate warehouse picking file with product numbers, customer details, shipping details (express of standard)
